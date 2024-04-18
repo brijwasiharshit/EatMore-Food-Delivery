191c08ca16036616fd0b63fffbf08884e77a8a52
brijwasiharshit <110799092+brijwasiharshit@users.noreply.github.com>
2024-04-18 17:50:10 +0530

    cors error

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,9 +1,9 @@
-import ResCard, { FastDelivery } from "./ResCard";
-import { useEffect, useState } from "react";
-import Shimmer from "./Shimmer";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { API_URL } from "../utils/constants";
 import useOnlineStatus from "../utils/useOnlineStatus";
+import ResCard, { FastDelivery } from "./ResCard";
+import Shimmer from "./Shimmer";
 
 const Body = () => {
   const [listRestaurants, setRestaurants] = useState([]);
@@ -16,12 +16,20 @@ const Body = () => {
   }, []);
 
   const fetchData = async () => {
-    const response = await fetch(`https://thingproxy.freeboard.io/fetch/${API_URL}`);
-    const json = await response.json();
-
-    const restaurants = json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants || [];
-    setRestaurants(restaurants);
-    setfilteredListRestaurants(restaurants);
+    try {
+      const response = await fetch(API_URL);
+      if (!response.ok) {
+        throw new Error('Failed to fetch data');
+      }
+      const json = await response.json();
+    
+      const restaurants = json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants || [];
+      setRestaurants(restaurants);
+      setfilteredListRestaurants(restaurants);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+      throw new Error('Unable to fetch data. Please install a CORS plugin to allow cross-origin requests. Link - https://chromewebstore.google.com/detail/allow-cors-access-control/lhobafahddgcelffkeicbaginigeejlf');
+    }
   };
 
   const onlineStatus = useOnlineStatus();
@@ -34,21 +42,50 @@ const Body = () => {
   ) : (
     <div className="body">
       <div className="border-4">
-        <input className="border-2 border-black mx-4 my-2 rounded-m " value={searchText} style={{ marginLeft: '10px' }} onChange={(e) => { setSearchText(e.target.value) }} />
-        <button className="bg-gray-600 px-4 rounded-lg text-white" onClick={() => {
-          const filteredList = listRestaurants.filter((res) => res.info.name.toLowerCase().includes(searchText.toLowerCase()));
-          setfilteredListRestaurants(filteredList);
-        }}>Search</button>
+        <input
+          className="border-2 border-black mx-4 my-2 rounded-m "
+          value={searchText}
+          style={{ marginLeft: "10px" }}
+          onChange={(e) => {
+            setSearchText(e.target.value);
+          }}
+        />
+        <button
+          className="bg-gray-600 px-4 rounded-lg text-white"
+          onClick={() => {
+            const filteredList = listRestaurants.filter((res) =>
+              res.info.name.toLowerCase().includes(searchText.toLowerCase())
+            );
+            setfilteredListRestaurants(filteredList);
+          }}
+        >
+          Search
+        </button>
 
-        <button className="bg-gray-600 px-4 rounded-lg mx-4 text-white" onClick={() => {
-          const filteredList = listRestaurants.filter((res) => res.info.avgRating > 4.2);
-          setfilteredListRestaurants(filteredList);
-        }}>Top Rated Restaurants</button>
+        <button
+          className="bg-gray-600 px-4 rounded-lg mx-4 text-white"
+          onClick={() => {
+            const filteredList = listRestaurants.filter(
+              (res) => res.info.avgRating > 4.2
+            );
+            setfilteredListRestaurants(filteredList);
+          }}
+        >
+          Top Rated Restaurants
+        </button>
       </div>
       <div className="flex flex-wrap">
-        {filteredListRestaurants.map(restaurant => (
-          <Link className="nav-link" key={restaurant.info.id} to={"./restaurants/" + restaurant.info.id}>
-          {restaurant?.info?.sla?.deliveryTime <= 25 ? <FastDeliveryCard resData={restaurant} /> : <ResCard key={restaurant.info.id} resData={restaurant} />}
+        {filteredListRestaurants.map((restaurant) => (
+          <Link
+            className="nav-link"
+            key={restaurant.info.id}
+            to={"./restaurants/" + restaurant.info.id}
+          >
+            {restaurant?.info?.sla?.deliveryTime <= 25 ? (
+              <FastDeliveryCard resData={restaurant} />
+            ) : (
+              <ResCard key={restaurant.info.id} resData={restaurant} />
+            )}
           </Link>
         ))}
       </div>
